4a408a2df0259360f1217d2ab322b0d62f3aedfb
sarn <sarn@tyndale.com>
2017-11-02 09:34:24 -0500

    added comments to main

---
+++ FitnessFrogConsoleApp/FitnessFrogConsoleApp/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FitnessFrogConsoleApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //prompt the user for minutes excercised
+            //add minutes excercised to total
+            //displat total minutes excercised to the screen
+            //repeat until the user quits
+        }
+    }
+}
