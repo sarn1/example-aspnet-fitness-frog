a8854967a3671da1b070c7470799fad3d43be8bf
sarn <sarn@tyndale.com>
2017-11-02 16:04:23 -0500

    basic app completion

--- FitnessFrogConsoleApp/FitnessFrogConsoleApp/Program.cs
+++ FitnessFrogConsoleApp/FitnessFrogConsoleApp/Program.cs
@@ -1,15 +1,36 @@
-﻿namespace FitnessFrogConsoleApp
+﻿using System;  //so we don't have to write "system" for System.Console.ReadLine();
+
+namespace FitnessFrogConsoleApp
 {
     class Program
     {
         static void Main()
         {
-            //prompt the user for minutes excercised
+            int total = 0;
+            bool keep_going = true;
 
+            while (keep_going)
+            {
+                //prompt the user for minutes excercised
+                Console.Write("Enter how many minutes you excercise or type 'quit': ");
+                string entry = Console.ReadLine();
 
-            //add minutes excercised to total
-            //displat total minutes excercised to the screen
-            //repeat until the user quits
+                //repeat until the user quits
+                if (entry == "quit")
+                {
+                    keep_going = false;
+                } else
+                {
+                    //add minutes excercised to total
+                    total += int.Parse(entry);
+
+                    //display total minutes excercised to the screen
+                    Console.WriteLine("You've entered a total of " + total + " minutes.");
+                }   
+            }
+
+            Console.WriteLine("Goodbye!");
+            Console.ReadLine();
         }
     }
 }
--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # example-fitness-frog
 Example console app written in C#
+- C# -> Windows Classic Desktop -> Console
+- REPL = scratch paper.  Open by going to View -> Other Windows -> C# Interactive Window
